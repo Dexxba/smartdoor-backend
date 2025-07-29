7f357c5a491bf27e3f619a6b25a4ed8cbaa81476
Dexxba <bogdanov.daniel@yahoo.com>
2025-07-29 13:29:12 +0100

    Initial Spring Boot backend project

---
+++ src/main/java/com/smartdoor/backend/SmartdoorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartdoor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartdoorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartdoorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartdoor/backend/SmartdoorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartdoor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdoorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
